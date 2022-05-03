cc22bb5c30fd12e43a9cb0b8441ca8a18ae00ecd
PreranaMalge <preranamalge123@gmail.com>
2022-05-03 22:23:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,20 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //var sum=5;
+  var name='Newton School'
+  var num1=5;
+  var num2=6;
+  let obj={
+    name:'name',
+    age:30
+  }
+  var arr=[1,2,3,4,5];
+
+  const fn =() => {
+    console.log("hi");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h2>{name}</h2>
+        Sum={5+6}
+        temp={num1+num2}
+        {
+          // obj={obj}
+          // arr={arr}
+        }
+
+        {/* {{js code}} */}
+
+        {fn()}
+        {/* {{if-else}} */}
+
+        {arr.map((item)=>(
+          <div>{item}</div>
+        ))}
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.newtonschool.co/"
           target="_blank"
-          rel="noopener noreferrer"
+          
         >
-          Learn React
+          Newton School Website
         </a>
       </header>
     </div>
@@ -23,3 +54,5 @@ function App() {
 }
 
 export default App;
+
+// JSX - Javascript XML - //templating
